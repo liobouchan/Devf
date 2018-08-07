44072d26a1f137c85447f300be6dc214c86d4c21
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-06 19:41:15 -0500

    model export user.js

--- AirBnb/src/models/user.js
+++ AirBnb/src/models/user.js
@@ -59,9 +59,13 @@ UserSchema.pre('save',(next)=>{
   if (!user.isModified('password')) { return next(); }
 
   bcrypt.genSalt(SALT , (err,salt)=>{
+    if (err) return next(err);
     bcrypt.hash(user.password, salt, (err, hash)=>{
+      if (err) return next(err);
       user.password = hash;
       next();
     });
   });
 });
+
+export default mongoose.model('Users', UserSchema);
