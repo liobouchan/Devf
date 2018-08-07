ae7915e190991b695615d939bf92c8fe76e4ccf7
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-06 20:34:47 -0500

    Index.js modified to connect to DBMongo

--- AirBnb/index.js
+++ AirBnb/index.js
@@ -4,6 +4,16 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 const app = express();
+const PORT = process.env.PORT || 3000
+const mongoURI = process.env.MONGODB_URI || "mongodb://admin123:admin123@ds215172.mlab.com:15172/bgsg"
+
+mongoose.connect(mongoURI,{useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', ()=> console.log('Error en conectar a la base de Datos'))
+  .once('open', ()=> console.log('Conectado a la BD'))
+  
+app.use(parser.json());
+app.use(cors());
 
 app.get('/', (req , res)=>{
   res.send('Server on');
