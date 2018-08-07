402eb3ea2ddb6b3b7ba24f903aa5b4ee26326bd4
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-07 14:49:37 -0500

    Index.js of SD have been modified to use body-parser, cors, and connect with MLAB

--- SinDelantal/index.js
+++ SinDelantal/index.js
@@ -1,5 +1,22 @@
 import express from 'express'
+import parser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express();
+const PORT = process.env.PORT || 3000
+const mongoURI = process.env.MONGODB_URI || "mongodb://admin123:admin123@ds133311.mlab.com:33311/bringeit"
+
+mongoose.db = connect(mongoURI,{useNewUrlParser: true} );
+const db = mongoose.connection;
+db.on('error', ()=> console.log('Eror en conectar a la base de datos'))
+  .once('open', ()=> console.log("Conectado a la Base De Datos"))
+
+app.use(parser.json());
+app.use(cors());
+
+app.get('/', (req , res)=>{
+  res.send('Server on');
+});
 
 app.listen(3000, () => console.log('Server on 3000'));
