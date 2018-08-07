2beeb2731c12e7d407503cf61af28e838f3db469
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-06 20:02:38 -0500

    Propiedades Schema done

---
+++ AirBnb/src/models/propiedades.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+const PropiedadesSchema = new Schema({
+  "nombre":{
+    type: String,
+    required: true
+  },
+  "descripcion_corta":{
+    type: String,
+    required: true
+  },
+  "descripcion_larga":{
+    type: String,
+    required: true
+  },
+  "ubicacion":{
+    type: String,
+    required: true
+  },
+  "pais":{
+    type: String,
+    required: true
+  }
+  "user":{
+    type: Schema.Types.ObjectId,
+    ref: 'Users'
+    required: true
+  },
+  "tipo":{
+    type: Number,
+    required: true
+  },
+  "precio":{
+    type: Number,
+    required: true
+  },
+  "calificacion":{
+    type: [{
+      "comentario" : String,
+      "estrellas" : Number
+    }],
+    required: true
+  },
+  "caracteristicas":[{
+    type: Schema.Types.ObjectId,
+    ref: 'Caracteristicas',
+    required: true
+  }],
+  "servicios":[{
+    type: Schema.Types.ObjectId,
+    ref: 'Servicios',
+    required: true
+  }],
+  "fotos" : []
+},{collection: 'Propiedades', timestamps:true});
+
+export default mongoose.model('Propiedades',PropiedadesSchema);
