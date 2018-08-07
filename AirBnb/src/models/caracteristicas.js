c0d2b9e73f1f515ad269e392e6b230dbd0bc531b
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-06 19:49:30 -0500

    Servicios Schema created

--- AirBnb/src/models/caracteristicas.js
+++ AirBnb/src/models/caracteristicas.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
-const caracteristicas = new Schema({
+const CaracteristicasSchema = new Schema({
   "nombre":{
     type: String,
     required: true
