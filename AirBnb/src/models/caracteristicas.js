d3d6ca92932b87b0126f27b7e2137a17ab94cf43
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-06 19:47:18 -0500

    Caracteristicas Schema

---
+++ AirBnb/src/models/caracteristicas.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+const caracteristicas = new Schema({
+  "nombre":{
+    type: String,
+    required: true
+  },
+  "descripcion":{
+    type: String,
+    required: true
+  },
+  "numero_personas"{
+    type: Number,
+    required: true
+  }
+},{collection: "Caracteristicas", timestamps:true});
+
+export default mongoose.model('Caracteristicas',CaracteristicasSchema);
