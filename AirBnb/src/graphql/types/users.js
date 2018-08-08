76bc2a9cd7639c34f9d7c44cf512d8edd76f9f27
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-07 19:57:47 -0500

    user.js to GraphQL types

---
+++ AirBnb/src/graphql/types/users.js
@@ -0,0 +1,47 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLNonNull
+} from 'graphql'
+
+export const UserType = new GraphQLObjectType({
+  name: 'Usuarios',
+  description: 'Usuarios de la Base de Datos'
+  fields: () => ({
+    _id: {
+      type : GraphQLNonNull(GraphQLID)
+    },
+    nombre: {
+      type : GraphQLString
+    },
+    apellidos: {
+      type : GraphQLString
+    },
+    correo: {
+      type : GraphQLString
+    },
+    fecha_nacimiento: {
+      type : GraphQLString
+    },
+    username: {
+      type : GraphQLString
+    },
+    genero: {
+      type : GraphQLString
+    },
+    pais: {
+      type : GraphQLString
+    },
+    ubicacion: {
+      type : GraphQLString
+    },
+    rol: {
+      type : GraphQLString
+    },
+    foto: {
+      type : GraphQLString
+    },
+  })
+});
