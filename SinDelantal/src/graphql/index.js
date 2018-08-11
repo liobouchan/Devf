b4b1d6890458e7a40eb6f5c6f0635e94828c9a06
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-10 19:56:33 -0500

    create index.js on the directory graphql on SD

---
+++ SinDelantal/src/graphql/index.js
@@ -0,0 +1,10 @@
+import queries from './queries'
+import {GraphQLObjectType,
+        GraphQLSchema}from 'graphql'
+
+export default new GraphQLSchema({
+    query = new GraphQLObjectType({
+        name: 'Query',
+        fields: queries
+    })
+});
