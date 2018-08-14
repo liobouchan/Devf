52d75ce1971ff8d89555e60ae3e32a9690732b7d
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-14 11:34:00 -0500

    Mutation to update reservacion has been done

--- AirBnb/src/graphql/mutations/reservaciones/index.js
+++ AirBnb/src/graphql/mutations/reservaciones/index.js
@@ -1,7 +1,9 @@
 import addReservaciones from './add'
 import deleteReservacion from './delete'
+import updateReservacion from './update'
 
 export default{
     addReservaciones,
-    deleteReservacion
+    deleteReservacion,
+    updateReservacion
 }
