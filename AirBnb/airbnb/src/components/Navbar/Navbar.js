06b95a5b48b6dc142c75dcf4dba8ceccd1788c33
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-17 19:18:11 -0500

    modding the collapse button

--- AirBnb/airbnb/src/components/Navbar/Navbar.js
+++ AirBnb/airbnb/src/components/Navbar/Navbar.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 
 class Navbar extends Component {
+
+   
+
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Airbnb Clone</a>
-                <button className="navbar-toggle" type="button" data-toggle="collapse"
-                    data-target="collapseAirbnb">
-                    <span className="navbar-toggle-icon"></span>
+                <a  className="navbar-brand" href="#">Airbnb Clone</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseAirbnb" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="collapseAirbnb">
@@ -15,18 +18,24 @@ class Navbar extends Component {
                         <li className="nav-item">
                             <a className="nav-link">Propiedades</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item"> 
                             <a className="nav-link">Login</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item"> 
                             <a className="btn btn-success">Sign Up</a>
                         </li>
                     </ul>
+
                 </div>
 
             </nav>
         )
+
+
     }
+
+
+
 }
 
 export default Navbar;
