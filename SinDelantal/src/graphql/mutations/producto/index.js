f60f13ecc1ec3ff9d99428acad90605595366c7f
Lio Bouchan <liogothicvampire@gmail.com>
2018-08-26 22:31:11 -0500

    Create the update of Product mutation on SD

--- SinDelantal/src/graphql/mutations/producto/index.js
+++ SinDelantal/src/graphql/mutations/producto/index.js
@@ -1,8 +1,9 @@
 import addProducto from './add'
 import deleteProducto from './delete'
-
+import updateProducto from './update'
 
 export default{
     addProducto,
-    deleteProducto
+    deleteProducto,
+    updateProducto
 }
